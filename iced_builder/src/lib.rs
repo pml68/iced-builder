27fcc7a6a3dee7777449c0517a11838310aa164d
pml68 <contact@pml68.me>
2024-10-26 22:22:45 +0200

    feat: implement `std::error::Error` for custom `Error` enum

--- iced_builder/src/lib.rs
+++ iced_builder/src/lib.rs
@@ -8,41 +8,27 @@ use types::{
     element_name::ElementName, project::Project, rendered_element::RenderedElement, DesignerPage,
 };
 
-#[derive(Debug, Clone)]
-pub enum Error {
-    IOError(std::io::ErrorKind),
-    SerdeError(String),
-    FormatError(String),
-    NonExistentElement,
-    DialogClosed,
-    String(String),
-}
+use thiserror::Error;
 
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::SerdeError(string) | Self::FormatError(string) | Self::String(string) => {
-                write!(f, "{}", string)
-            }
-            Self::IOError(kind) => {
-                write!(f, "{}", kind)
-            }
-            Self::NonExistentElement => {
-                write!(f, "The element tree contains no matching element.")
-            }
-            Self::DialogClosed => {
-                write!(
-                    f,
-                    "The file dialog has been closed without selecting a valid option."
-                )
-            }
-        }
-    }
+#[derive(Debug, Clone, Error)]
+pub enum Error {
+    #[error("an IO error accured: {0}")]
+    IOError(String),
+    #[error("a Serde error accured: {0}")]
+    SerdeError(String),
+    #[error("an RustFmt error accured: {0}")]
+    FormatError(String),
+    #[error("the element tree contains no matching element")]
+    NonExistentElement,
+    #[error("the file dialog has been closed without selecting a valid option")]
+    DialogClosed,
+    #[error("{0}")]
+    String(String),
 }
 
 impl From<std::io::Error> for Error {
     fn from(value: std::io::Error) -> Self {
-        Self::IOError(value.kind())
+        Self::IOError(value.to_string())
     }
 }
 
